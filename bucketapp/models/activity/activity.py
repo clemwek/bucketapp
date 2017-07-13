39a1b33791e84e9072d9ac44a332abbfa5f257e5
Clement _dhwty_ <clemwek@gmail.com>
2017-07-13 12:46:24 +0300

    Add id property to the model

--- bucketapp/models/activity/activity.py
+++ bucketapp/models/activity/activity.py
@@ -1,7 +1,11 @@
+import uuid
+
+
 class Activity(object):
-    def __init__(self, name, bucketlist_id, description, date, status):
+    def __init__(self, name, bucketlist_id, description, date, status, id=None):
         self.name = name
         self.description = description
         self.bucketlist_id = bucketlist_id
         self.date = date
         self.status = status
+        self.id = uuid.uuid4().hex if id is None else id
--- bucketapp/models/bucketlist/bucketlist.py
+++ bucketapp/models/bucketlist/bucketlist.py
@@ -1,5 +1,9 @@
+import uuid
+
+
 class Bucketlist(object):
-    def __init__(self, name, user_id):
+    def __init__(self, name, user_id, id=None):
         self.name = name
         self.user_id = user_id
+        self.id = uuid.uuid4().hex if id is None else id
         self.activities = {}
