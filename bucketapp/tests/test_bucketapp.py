0cbfe84679ea063ced77a807639ec07314330141
Clement _dhwty_ <clemwek@gmail.com>
2017-07-18 16:39:11 +0300

    Test add edit and delete bucketlist

--- bucketapp/tests/test_bucketapp.py
+++ bucketapp/tests/test_bucketapp.py
@@ -1,5 +1,5 @@
 import unittest
-from bucketapp.bucketapp import app, get_id_for_email
+from bucketapp.bucketapp import app, get_id_for_email, add_bucket_list, rm_bucket_list, edit_bucket_list
 
 
 class TestBucketListApp(unittest.TestCase):
@@ -7,21 +7,18 @@ class TestBucketListApp(unittest.TestCase):
         self.client = app.test_client(self)
 
     def register(self, name, email, password):
-        # client = app.test_client(self)
-        with self.client:
-            return self.client.post(
-                '/register',
-                data=dict(inputName=name, inputEmail=email, inputPassword=password, inputPasswordAgain="test"),
-                follow_redirects=True
-            )
+        return self.client.post(
+            '/register',
+            data=dict(inputName=name, inputEmail=email, inputPassword=password, inputPasswordAgain="test"),
+            follow_redirects=True
+        )
 
     def login(self, email, password):
-        with self.client:
-            return self.client.post(
-                '/login',
-                data=dict(inputEmail=email, inputPassword=password),
-                follow_redirects=True
-            )
+        return self.client.post(
+            '/login',
+            data=dict(inputEmail=email, inputPassword=password),
+            follow_redirects=True
+        )
 
     def logout(self):
         # client = app.test_client(self)
@@ -56,17 +53,28 @@ class TestBucketListApp(unittest.TestCase):
     def test_logout(self):
         response = self.logout()
         self.assertIn(b'you are logged out.', response.data)
+        # self.assertFalse(session['logged_in'])
 
-    def test_correct_login(self):
+    def test_login(self):
         self.register('test', 'test2@test.com', 'test')
         self.logout()
-
+        response = self.login('test@te.com', 'test')
+        self.assertIn(b'You do not have an account, please register.', response.data)
         response = self.login('test2@test.com', 'test')
         self.assertIn(b'You are logged in', response.data)
 
-    def test_incorrect_login(self):
-        response = self.login('test@te.com', 'test')
-        self.assertIn(b'You do not have an account, please register.', response.data)
+    def test_add_edit_delete_bucket_list(self):
+        add_bucket_list('test', 'abdfhs')
+        self.assertIn('abdfhs', app.bucketlist)
+        self.assertTrue(len(app.bucketlist['abdfhs']) == 1)
+
+        edit_bucket_list('test edit', 'abdfhs', list(app.bucketlist['abdfhs'].keys())[0])
+        self.assertEqual('test edit', app.bucketlist['abdfhs'][list(app.bucketlist['abdfhs'].keys())[0]].name)
+
+        add_bucket_list('test2', 'abdfhs')
+        init_bucket_count = len(app.bucketlist['abdfhs'])
+        rm_bucket_list(list(app.bucketlist['abdfhs'].keys())[0], 'abdfhs')
+        self.assertEqual(init_bucket_count - len(app.bucketlist['abdfhs']), 1)
 
 if __name__ == '__main__':
     unittest.main()
